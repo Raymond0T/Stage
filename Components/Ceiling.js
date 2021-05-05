59a7315a99be9415aa8855990ee6404dda411ff0
Raymond0T <36683390+Raymond0T@users.noreply.github.com>
2021-05-05 14:44:23 -0700

    Add files via upload
    made the timer for ceiling and screens

--- Components/Ceiling.js
+++ Components/Ceiling.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './Ceiling.css'
 import Logo from '../images/icon-logo.png';
 import Screen from './Screen.js';
@@ -6,11 +6,48 @@ import Screen from './Screen.js';
 
 
 function Ceiling(){
+
+    let [logoColor, setlogoColor] = useState(0);
+    let [temp, setTemp] = useState(0);
+
+    const option = ['25','55','125','250','350'];
+    const time = [100,1000,1000,2000,5000,1000]
+
+    useEffect(()=>{
+        let choice = Math.floor(Math.random() * (option.length - 1));
+        while(logoColor === choice){
+            choice = Math.floor(Math.random() * (option.length - 1));
+        }
+
+        if(temp === time.length - 1){
+            const interval_id = setTimeout(()=>{
+                setlogoColor(choice);
+                setTemp(temp = 0);
+            },time[temp]);
+            console.log(time[temp] + ' ceiling');
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+        else if(temp < time.length){
+            const interval_id = setTimeout(()=>{
+                setlogoColor(choice);
+                setTemp(temp+=1);
+            }, time[temp]);
+            console.log(time[temp] + ' ceiling');
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+    },[temp])
+   
+
     return(
         <div className="ceiling">
             <div className="ceiling__logo">
                 <img 
                     className="ceiling__logoImage"
+                    style={{filter:`invert(45%) sepia(81%) saturate(2000%) hue-rotate(${option[logoColor]}deg) brightness(98%) contrast(2000%) drop-shadow(0 0 1rem rgb(241, 241, 245))` }}
                     src={Logo}
                     alt=""
                 />
--- Components/Stage.js
+++ Components/Stage.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import './Stage.css';
 import Screen from './Screen.js';
 
 
 function Stage(){
+    const option = ['black','#2dd3e9','#c52bab','#5ddf59','#95a034'];
+    const time = [100,1000,1000,2000,5000,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,1000];
+    let [color, setColor] = useState(0);
+    let [temp, setTemp] = useState(0);
+    useEffect(() =>{
+        /*
+
+    function ColorChange(){
+        let colorChoice = Math.floor(Math.random() * (option.length-1));
+        while(colorChoice === color){
+            colorChoice = Math.floor(Math.random() * (option.length-1));
+        }
+        setColor(colorChoice);
+    }
+
+        */
+        let colorChoice = Math.floor(Math.random() * (option.length-1));
+        while(colorChoice === color){
+            colorChoice = Math.floor(Math.random() * (option.length-1));
+        }
+        if(temp === time.length - 1){
+            const interval_id = setTimeout(()=>{
+                setColor(colorChoice);
+                setTemp(temp=0);
+            },time[temp]);
+            console.log(time[temp] + ' stage');
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+        else if(temp < time.length){
+            const interval_id = setTimeout(()=>{
+                setColor(colorChoice);
+                setTemp(temp += 1);
+            },time[temp]);
+            console.log(time[temp] + ' stage');
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+    },[temp]);
+
+
     return(
         <div className="stage">
             <div className="stage__mainDisplay">
-                <Screen screenWidth="300px" screenHeight="150px" logoWidth="200px" logoHeight="200px" radius="2px" background="black" shadow=" 0 0 20px lightblue"/>
+                <Screen screenWidth="300px" screenHeight="150px" logoWidth="200px" logoHeight="200px" radius="2px" background={option[color]} color={option[color]} shadow=" 0 0 20px "/>
             </div>
             <div className="stage__leftDisplay">
-                <Screen screenWidth="200px" screenHeight="125px" logoWidth="150px" logoHeight="125px" radius="2px" background="black" shadow=" 0 0 20px lightblue"/>
+                <Screen screenWidth="200px" screenHeight="125px" logoWidth="150px" logoHeight="125px" radius="2px" background={option[color]} color={option[color]} shadow=" 0 0 20px "/>
             </div>
             <div className="stage__rightDisplay">
-                <Screen screenWidth="200px" screenHeight="125px" logoWidth="150px" logoHeight="125px" radius="2px" background="black" shadow=" 0 0 20px lightblue"/>
+                <Screen screenWidth="200px" screenHeight="125px" logoWidth="150px" logoHeight="125px" radius="2px" background={option[color]} color={option[color]} shadow=" 0 0 20px "/>
             </div>
             <div className="stage__booth">
-               <Screen screenWidth="150px" screenHeight="30px" logoWidth="100px" logoHeight="30px" radius="2px" background="black" shadow=" 0 0 20px lightblue"/>
+               <Screen screenWidth="150px" screenHeight="30px" logoWidth="100px" logoHeight="30px" radius="2px" background={option[color]} color={option[color]} shadow=" 0 0 20px "/>
             </div>
         </div>
     );
