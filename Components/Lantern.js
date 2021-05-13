a01a4e5334d1cba1565ffacb9a54fdc1efbb05bd
Raymond0T <36683390+Raymond0T@users.noreply.github.com>
2021-05-13 16:55:04 -0700

    Add files via upload

--- Components/Lantern.js
+++ Components/Lantern.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './Lantern.css';
 import Glare from'../images/glare.png';
 
-function Lantern({posVert, posHori, color, showRay, angle}){
+function Lantern({posVert, posHori, color, angle}){
 
     let match= {
         1: 'c7750a',
@@ -12,26 +12,54 @@ function Lantern({posVert, posHori, color, showRay, angle}){
         150: '0b96d6',
         250: 'b10ed1'
     }
+    
+    
+    const rayQueue = ['none','none','none','none','none','none','none','none','none','none','none','none','none','none','none'];
+    const sideRayQueue = ['none','none','block','none','block','none','block','none','block','none','block','none','block','none','none','none'];
+    const time = [10,50000,20000,2000,2000,2000,2000,2000,2000,2000,2000,2000,2000];
 
-    let change = match[color];
+    // 50se, 30 sec
+    //let [angle, setAngle] = useState(0);
+    
+    let [flash, setFlash] = useState(0);
+
+    useEffect(()=>{
+
+        if(flash === time.length - 1){
+            let interval_id = setTimeout(()=>{
+                setFlash(flash= 0); 
+            },time[flash])
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+
+        else if(flash < time.length){
+            let interval_id = setTimeout(()=>{
+                console.log(flash + ' check ' + time[flash]);
+                setFlash(flash+=1);
+            }, time[flash])
+            return()=>{
+                clearInterval(interval_id);
+            }
+        }
+    },[flash])
 
     return(
         <div className="lantern">
-            <div className="lantern__bulb">
+            <div className="lantern__bulb" style={{display: angle===90 ? rayQueue[flash] : sideRayQueue[flash]}}>
                 <img
                     className="lantern__glare"
-                    
                     src={Glare}
                     style={{
-                        display: showRay,
-                        filter: `invert(43%) sepia(81%) saturate(1412%) hue-rotate(${color}deg) brightness(98%) contrast(96%) drop-shadow(0 0 1rem blue)`,}}
+                        filter: `invert(43%) sepia(81%) saturate(1412%) hue-rotate(${color}deg) brightness(98%) contrast(96%) drop-shadow(0 0 1rem blue)`}}
                     alt=""
                 />
-                <div className="lantern__ray" style={{
-                    display: showRay,
+                <div className={(flash !== 2) ? "lantern__ray" : "lantern__rainbow"} style={{
+                    display: (angle===90) ? rayQueue[flash] : sideRayQueue[flash],
                     top:posVert,
                     right:posHori, 
-                    background: `linear-gradient(to right, #${change} 1%, transparent)`, 
+                    background: `linear-gradient(to right, #${match[color]} 1%, transparent)`, 
                     transform:`perspective(100px) rotate(${angle}deg) rotateY(120deg)`}}>
                 </div>
             </div>
